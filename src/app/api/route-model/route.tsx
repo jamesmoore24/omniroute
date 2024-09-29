2e8f678c19e1f8c940f30e9547652e92fc022ab1
jamesmoore24 <jame.moore24@gmail.com>
2024-09-29 13:23:19 -0400

    deploy build with hardcoded port

--- src/app/api/route-model/route.tsx
+++ src/app/api/route-model/route.tsx
@@ -3,21 +3,18 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { message, expensive_model, cheap_model } = await req.json();
 
-  const response = await fetch(
-    `http://${process.env.ROUTING_SERVER_IP}:${process.env.ROUTING_SERVER_PORT}/route`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: message,
-        threshold: 0.1,
-        strong_model: expensive_model,
-        weak_model: cheap_model,
-      }),
-    }
-  );
+  const response = await fetch(`http://0.0.0.0:8000/route`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: message,
+      threshold: 0.1,
+      strong_model: expensive_model,
+      weak_model: cheap_model,
+    }),
+  });
 
   const data = await response.json();
 
