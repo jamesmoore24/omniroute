3c49a6cb00829fdbeb0d82f66fd62e82d06f937d
jamesmoore24 <jame.moore24@gmail.com>
2024-10-10 14:10:56 -0400

    remoed linting errors

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Job } from "@/types";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Header } from "@/components/Header";
+import JobPosting from "@/components/Careers/JobPosting";
 
 const jobs: Job[] = [
   {
@@ -16,6 +17,11 @@ export default function Component() {
     <TooltipProvider>
       <div className="flex flex-col h-screen bg-gray-100 text-black">
         <Header />
+        <div className="flex-1 p-4">
+          {jobs.map((job) => (
+            <JobPosting key={job.id} job={job} />
+          ))}
+        </div>
       </div>
     </TooltipProvider>
   );
